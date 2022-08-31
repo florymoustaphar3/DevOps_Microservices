84c16dc5efbef0c01b866fc342c1b8d9954a9641
Flory Moustaphar <florymoustaphar3@gmail.com>
2022-08-31 20:19:39 +0200

    Update run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
-
+!/usr/bin/env bash
+  
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=moustaphar/project4:v8
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flask-app --image=moustaphar/project4:v8 --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+sleep 100
+kubectl port-forward flask-app 8000:80
